a5bd5b76136404d05e1f557dbe75c33568e5a898
Raffaele <raffaele.trivillin@stud.itsaltoadriatico.it>
2024-03-15 14:07:15 +0100

    Created project with minimal API, Defined endpoints and Data Access Layer

---
+++ CLED.OnlineShop.API/Endpoints/CustomersEndpoints.cs
@@ -0,0 +1,74 @@
+﻿using CLED.OnlineShop.API.Models;
+using CLED.OnlineShop.API.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace CLED.OnlineShop.API.Endpoints
+{
+    public static class CustomersEndpoints
+    {
+        // extension method per spostare fuori dal program.cs la configurazione degli endpoint
+        public static IEndpointRouteBuilder MapCustomerEndpoints(this IEndpointRouteBuilder builder)
+        {
+            var group = builder.MapGroup("api/v1/customers")
+                .WithTags("Customers")
+                .WithOpenApi();
+
+            group.MapGet("/", async (CustomerService data) => await GetCustomersAsync(data)).WithName("GetCustomers")
+            .WithSummary("Get all customers")
+            .WithDescription("Returns the complete customer list");
+
+            //group.MapGet("/{id:int}", async (int id, CustomerService data) => await GetCustomerAsync(id, data));\
+            group.MapGet("/{id:int}", GetCustomerAsync)
+                .WithName("GetCustomer");
+
+            group.MapPost("/", InsertCustomerAsync)
+                .WithName("InsertCustomer")
+                .WithSummary("Creates the customer");
+
+            group.MapPut("/{id:int}", UpdateCustomerAsync)
+                .WithName("UpdateCustomer")
+                .WithSummary("Updates the customer");
+
+            group.MapDelete("/{id:int}", DeleteCustomerAsync)
+                .WithName("DeleteCustomer")
+                .WithSummary("Deletes the customer");
+
+            return builder;
+        }
+
+        private static async Task<Ok<IEnumerable<Customer>>> GetCustomersAsync(CustomerService data)
+        { 
+            // typed results è più comodo perchè ci permette di restituire un oggetto con un messaggio di errore
+            return TypedResults.Ok(await data.GetCustomersAsync());
+        }
+        private static async Task<Results<Ok<Customer>, NotFound>> GetCustomerAsync(int id, CustomerService data)
+        {
+            var customer = await data.GetCustomerAsync(id);
+            if (customer is null)
+                return TypedResults.NotFound();
+            return TypedResults.Ok(customer);
+        }
+        private static async Task<Created> InsertCustomerAsync(Customer customer, CustomerService data)
+        {
+            await data.InsertCustomerAsync(customer);
+            return TypedResults.Created();
+        }
+
+        // Results mi permette di elencare le possibili risposte che posso ottenere
+        private static async Task<Results<NoContent, NotFound>> UpdateCustomerAsync(int id, Customer customer, CustomerService data)
+        {
+            if (await data.GetCustomerAsync(id) is null) 
+                return TypedResults.NotFound();
+            
+            await data.UpdateCustomerAsync(id, customer);
+            return TypedResults.NoContent();
+        }
+        private static async Task<Results<NoContent, NotFound>> DeleteCustomerAsync(int id, CustomerService data)
+        {
+            if (await data.GetCustomerAsync(id) is null)
+                return TypedResults.NotFound();
+            await data.DeleteCustomerAsync(id);
+            return TypedResults.NoContent();
+        }
+    }
+}
---
+++ CLED.OnlineShop.API/Endpoints/OrdersEndpoint.cs
@@ -0,0 +1,71 @@
+﻿using CLED.OnlineShop.API.Models;
+using CLED.OnlineShop.API.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace CLED.OnlineShop.API.Endpoints
+{
+    public static class OrdersEndpoint
+    {
+        public static IEndpointRouteBuilder MapOrdersEndpoints(this IEndpointRouteBuilder builder)
+        {
+            var group = builder.MapGroup("api/v1/orders")
+                .WithTags("Orders")
+                .WithOpenApi();
+
+            group.MapGet("/", async (OrderService data) => await GetOrdersAsync(data)).WithName("GetOrders")
+            .WithSummary("Get all Orders")
+            .WithDescription("Returns the complete Order list");
+
+            group.MapGet("/{id:int}", GetOrderAsync)
+                .WithName("GetOrder");
+
+            group.MapPost("/", InsertOrderAsync)
+                .WithName("InsertOrder")
+                .WithSummary("Creates the Order");
+
+            group.MapPut("/{id:int}", UpdateOrderAsync)
+                .WithName("UpdateOrder")
+                .WithSummary("Updates the Order");
+
+            group.MapDelete("/{id:int}", DeleteOrderAsync)
+                .WithName("DeleteOrder")
+                .WithSummary("Deletes the Order");
+
+            return builder;
+        }
+        private static async Task<Ok<IEnumerable<Order>>> GetOrdersAsync(OrderService data)
+        {
+            // typed results è più comodo perchè ci permette di restituire un oggetto con un messaggio di errore
+            return TypedResults.Ok(await data.GetOrdersAsync());
+        }
+        private static async Task<Results<Ok<Order>, NotFound>> GetOrderAsync(int id, OrderService data)
+        {
+            var Order = await data.GetOrderAsync(id);
+            if (Order is null)
+                return TypedResults.NotFound();
+            return TypedResults.Ok(Order);
+        }
+        private static async Task<Created> InsertOrderAsync(Order Order, OrderService data)
+        {
+            await data.InsertOrderAsync(Order);
+            return TypedResults.Created();
+        }
+
+        // Results mi permette di elencare le possibili risposte che posso ottenere
+        private static async Task<Results<NoContent, NotFound>> UpdateOrderAsync(int id, Order Order, OrderService data)
+        {
+            if (await data.GetOrderAsync(id) is null)
+                return TypedResults.NotFound();
+
+            await data.UpdateOrderAsync(id, Order);
+            return TypedResults.NoContent();
+        }
+        private static async Task<Results<NoContent, NotFound>> DeleteOrderAsync(int id, OrderService data)
+        {
+            if (await data.GetOrderAsync(id) is null)
+                return TypedResults.NotFound();
+            await data.DeleteOrderAsync(id);
+            return TypedResults.NoContent();
+        }
+    }
+}
