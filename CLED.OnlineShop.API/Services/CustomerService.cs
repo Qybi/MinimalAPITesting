ce24dff96694e2525d20d0424f4289eddbb16174
Raffaele <raffaele.trivillin@stud.itsaltoadriatico.it>
2024-03-15 14:12:51 +0100

    Reapply "Created project with minimal API, Defined endpoints and Data Access Layer"
    This reverts commit f08a94a4a43152c820d694cd58bca9d6404515d7.

---
+++ CLED.OnlineShop.API/Services/CustomerService.cs
@@ -0,0 +1,62 @@
+﻿using CLED.OnlineShop.API.Models;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace CLED.OnlineShop.API.Services
+{
+    public class CustomerService
+    {
+        private readonly string _connectionString;
+        public CustomerService(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("db");
+        }
+
+        public async Task<IEnumerable<Customer>> GetCustomersAsync()
+        {
+            using var connection = new MySqlConnection(_connectionString);
+            await connection.OpenAsync();
+            return await connection.QueryAsync<Customer>(@"SELECT userId as id,
+name,
+address,
+phone
+FROM Customers;");
+        }
+        public async Task<Customer> GetCustomerAsync(int id)
+        {
+            using var connection = new MySqlConnection(_connectionString);
+            await connection.OpenAsync();
+            return await connection.QueryFirstOrDefaultAsync<Customer>(@"SELECT userId as id,
+name,
+address,
+phone
+FROM Customers
+WHERE userId = @id;", new { id = id });
+        }
+
+        // le 3 virgolette nella stringa permettono di identare il testo
+        public async Task InsertCustomerAsync(Customer customer)
+        {
+            using var connection = new MySqlConnection(_connectionString);
+            await connection.OpenAsync();
+            await connection.ExecuteAsync("""
+                INSERT INTO Customers (name, address, phone) 
+                VALUES (@name, @address, @phone)
+                """, customer);
+        }
+
+        public async Task UpdateCustomerAsync(int id, Customer customer)
+        {
+            using var connection = new MySqlConnection(_connectionString);
+            await connection.OpenAsync();
+            await connection.ExecuteAsync(@"UPDATE Customers SET name = @name, address = @address, phone = @phone where userId = @id", customer);
+        }
+
+        public async Task DeleteCustomerAsync(int id)
+        {
+            using var connection = new MySqlConnection(_connectionString);
+            await connection.OpenAsync();
+            await connection.ExecuteAsync(@"DELETE FROM Customers WHERE userId = @id", new { id = id });
+        }   
+    }
+}
